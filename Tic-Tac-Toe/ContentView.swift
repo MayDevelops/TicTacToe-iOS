9e1219b37610d31026f107efdfd6f4995e4ff6c4
JC MAy <jcjamesmay@gmail.com>
2020-06-21 23:44:36 -0700

    Initial

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by JC May on 6/6/20.
+//  Copyright © 2020 JC May. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
